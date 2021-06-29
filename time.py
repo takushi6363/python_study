bf3e03ac6adec8e4f2e2d31ae7394e677d7557c8
takushi_akayama <akayama63@gmail.com>
2021-06-29 21:47:46 +0900

    Update time.py

--- time.py
+++ time.py
@@ -7,3 +7,11 @@ d = time.strftime("%Y/%m/%d %A",t)
 h = time.strftime("%H:%M:%S ,t")
 print(d)
 print(h)
+
+import datetime
+n = datetime.datetime.now()
+print(n)
+print("時を取り出す",n.hour)
+print("分を取り出す",n.minute)
+print("秒を取り出す",n.second)
+
